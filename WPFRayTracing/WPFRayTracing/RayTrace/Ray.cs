c3d96ca72ca43c9c5e58c1f32f1b11fcf9e3a5ee
Baesky <crushrush@qq.com>
2016-02-23 18:01:24 +0800

    增加ray

---
+++ WPFRayTracing/WPFRayTracing/RayTrace/Ray.cs
@@ -0,0 +1,36 @@
+﻿using MathNet.Spatial.Euclidean;
+
+namespace WPFRayTracing.RayTrace
+{
+    class Ray
+    {
+        public Point3D Origin { get; set; }
+
+        public Vector3D Direction { get; set; };
+             
+        public Ray()
+        {
+            Origin = new Point3D(0,0,0);
+            Direction = new Vector3D(0,0,0);
+        }
+
+        public Ray(ref Point3D o, ref Vector3D d)
+        {
+            Origin = o;
+            Direction = d;
+        }
+
+        public Ray(Ray r)
+        {
+            Origin = r.Origin;
+            Direction = r.Direction;
+        }
+
+        ~Ray()
+        {
+
+        }
+        
+            
+    }
+}
