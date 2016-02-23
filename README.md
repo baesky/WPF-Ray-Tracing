c943088eaf42b88abdeef31afb69f0964f6ca663
Baesky <crushrush@qq.com>
2016-02-23 12:21:54 +0800

    Init Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WPF-Ray-Tracing
 Ray Tracing Exercise
+
+## Dependencies:
+[MathNet.Numerics.3.11.0](http://www.mathdotnet.com/)
---
+++ WPFRayTracing/WPFRayTracing/Controls/PresentationControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPFRayTracing
+{
+    /// <summary>
+    /// 显示RenderTarget的控件
+    /// </summary>
+    class PresentationControl : Canvas
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            //BitmapImage img = new BitmapImage(new Uri("c:\\demo.jpg"));
+            //dc.DrawImage(img, new Rect(0, 0, img.PixelWidth, img.PixelHeight));
+        }
+    }
+}
